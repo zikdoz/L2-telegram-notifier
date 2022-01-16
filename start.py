8f18015d1a1c2a00f895efa66bdc64bb3acce6e4
Herald Gersdorff <heraldgersdorff@yandex.ru>
2022-01-16 21:45:25 +0300

    added & configured logger

--- start.py
+++ start.py
@@ -1,6 +1,30 @@
 from telebot import *
 
+import logging
+import os
+
+def configLogger():
+    main_logger = logging.getLogger()
+    
+    custom_formatter = logging.Formatter(fmt='[{asctime}] [ {levelname:^10s}] `{module}`: {message}', style='{')
+
+    os.makedirs(name='./logs/', exist_ok=True)
+
+    file_handler = logging.FileHandler(filename='./logs/bot.log', mode='w', encoding='utf-8')
+    file_handler.setFormatter(custom_formatter)
+    main_logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(custom_formatter)
+    main_logger.addHandler(console_handler)
+
+    main_logger.setLevel(logging.NOTSET)
+
 def main():
+    configLogger()
+
+    logging.info('firing up telegram bot..')
+    
     telebot = TeleBot()
     telebot.readConfig('tele-config.yaml')
 
